115074ec55727d4eac387b9453384211cbb50248
Perry <perryjhoffman@gmail.com>
2015-01-02 22:56:24 -0800

    Updating profile pic, adding social icons to about.

--- scripts/main.js
+++ scripts/main.js
@@ -9,11 +9,22 @@ $(function() {
         get: 'user',
         userId: 15000428,
         links: true,
-        limit: 12,
+        limit: 8,
         sortBy: 'most-recent',
         resolution: 'standard_resolution',
         template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
     });
+//    var feed = new Instafeed({
+//        clientId: '97ae5f4c024c4a91804f959f43f2635f',
+//        target: 'instafeed',
+//        get: 'tagged',
+//        tagName: 'photographyportfolio',
+//        links: true,
+//        limit: 8,
+//        sortBy: 'most-recent',
+//        resolution: 'standard_resolution',
+//        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+//    });
     feed.run();
 
 
@@ -36,23 +47,22 @@ $(function() {
         var content    = entry.contentSnippet;
         var link       = entry.link;
         var date       = entry.publishedDate;
-        console.log('entry ', entry);
-        var template = '<div class="blog-entry clearfix"><a href="' + link + '" target="_blank">' +
+        var template = '<div class="blog-entry clearfix">' +
             '<div class="blog-entry_thumb pull-left"><img src="' + image + '" /></div>' +
             '<div class="blog-entry_content pull-left">' +
             '<h5>' + title + '</h5>' +
-            '<span class="blog-entry_date">' + date + '</span>' +
+            '<span class="blog-entry_date">' + moment(date).format('LL') + '</span>' +
             '<p>' + content + '</p>' +
-            '<p class="continue-reading pull-right"><i class="fa fa-plus-square-o"></i>Read More</p>' +
+            '<a href="' + link + '" target="_blank" class="continue-reading pull-right"><i class="fa fa-plus-square-o"></i>Read More</a>' +
             '</div>' +
-            '</a></div>'
+            '</div>'
         appendEntry(template);
     }
 
     function initialize() {
               var feed = new google.feeds.Feed("http://tanyascorner.wordpress.com/feed");
 //        var feed = new google.feeds.Feed("http://blog.travelunderwriters.com/feed");
-        feed.setNumEntries(5);
+        feed.setNumEntries(3);
         feed.load(function(result) {
             if (!result.error) {
                 for (var i = 0; i < result.feed.entries.length; i++) {
